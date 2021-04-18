84071507971ecae575fe5a4d4c9d8c749d476a49
oxKNYxo <kkny950@gmail.com>
2021-04-19 02:14:09 +0800

    2.0

--- main.js
+++ main.js
@@ -1,15 +1,102 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
-const {joinUser, removeUser, findUser} = require('./users');
+
+let firebase = require("firebase");
+let express = require("express");
+let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
+const firebaseConfig = require("./FirebaseConfig.json");
+const { time } = require("console");
+const pathName = firebaseConfig.pathName
+let port = process.env.PORT || 3000;
 
 
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+server.listen(port, function() {
+  console.log("Server listening on port %d", port);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database()
+
+
+
+io.on("connection", function(socket) {
+  console.log("Connected and ready!");
+
+
+  socket.on("join", (data) => {
+    let userName = data.userName;
+    let roomName = data.roomName;
+    console.log(`${userName} joined room: ${roomName}`);
+
+    // new message
+    database.ref(`room/${roomName}`).on('child_added', function(childSnapshot, prevChildKey) {
+      if (!childSnapshot) return;
+      let msgData = childSnapshot.val();
+
+      socket.emit("show message", msgData);
+    });
+
+    // remove message
+    database.ref(`room/${roomName}`).on('child_removed', function(childSnapshot, prevChildKey) {
+      if (!childSnapshot) return;
+      let msgData = childSnapshot.val();
+
+      socket.emit("hide message", msgData);
+    });
+  });
+
+
+  // send message
+  socket.on("send message", (data) => {
+    let time = getTime()
+    database.ref(`room/${data.room}/${time.full}`).set({
+      id: time.full,
+      time: time.short,
+      author: data.author,
+      message: data.message,
+      image: data.image
+    });
+  });
 });
 
 
 
+function getTime() {
+  let time = new Date();
+  let year = time.getFullYear().toString().padStart(4, 0);
+  let month = time.getMonth().toString().padStart(2, 0);
+  let date = time.getDate().toString().padStart(2, 0);
+  let hour = time.getHours().toString().padStart(2, 0);
+  let min = time.getMinutes().toString().padStart(2, 0);
+  let sec = time.getSeconds().toString().padStart(2, 0);
+  let milSec = time.getMilliseconds().toString().padStart(3, 0);
 
-http.listen(process.env.PORT || 3000);
+  return {
+    full: `${year}-${month}-${date}_${hour}:${min}:${sec}_${milSec}`,
+    short: `${hour}:${min}:${sec}`
+  };
+}
+
+
+// write
+// database.ref("Test").set("TEST", function(error) {
+//   if (error) {
+//     console.log("Failed with error: " + error)
+//   } else {
+//     console.log("success")
+//   }
+// })
+
+// listener
+// database.ref().on('child_changed', function(childSnapshot, prevChildKey) {
+//   console.log(childSnapshot.val())
+// });
+
+// read once
+// database.ref("Test").once('value', (snapshot) => {
+//   console.log(snapshot.val());
+// });
