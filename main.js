afc318d469f1769bedf77aa67b9649571acf6269
oxKNYxo <kkny950@gmail.com>
2021-06-14 16:11:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -80,6 +80,9 @@ io.on("connection", function(socket) {
         message: data.message
       })
     );
+    hasImage(data.message).then(resolve => console.log(resolve),
+      error => console.log(error)
+    );
   });
 });
 
