7c6d2415808267e46e967346f39abc4116e7ab63
oxKNYxo <kkny950@gmail.com>
2021-06-14 15:20:44 +0800

    2.0

--- main.js
+++ main.js
@@ -52,18 +52,44 @@ io.on("connection", function(socket) {
 
   // send message
   socket.on("send message", (data) => {
-    let time = getTime()
+    let time = getTime();
     database.ref(`room/${data.room}/${time.full}`).set({
       id: time.full,
       time: time.short,
       author: data.author,
-      message: data.message,
-      image: data.image
+      message: data.message
     });
+    hasImage(data.message).then(
+      database.ref(`room/${data.room}/${time.full}`).set({
+        image: data.message
+      })
+    );
   });
 });
 
 
+
+function hasImage(url) {
+  return new Promise(function(resolve, reject) {
+    var timeout = 5000;
+    var timer, img = new Image();
+    img.onerror = img.onabort = function() {
+        clearTimeout(timer);
+        reject("error");
+    };
+    img.onload = function() {
+         clearTimeout(timer);
+         resolve("success");
+    };
+    timer = setTimeout(function() {
+        // reset .src to invalid URL so it stops previous
+        // loading, but doens't trigger new load
+        img.src = "//!!!!/noexist.jpg";
+        reject("timeout");
+    }, timeout); 
+    img.src = url;
+  });
+}
 
 function getTime() {
   let time = new Date();
