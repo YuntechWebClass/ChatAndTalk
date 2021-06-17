84294297d8d1cc364d768d5368869bc7d75daaca
oxKNYxo <kkny950@gmail.com>
2021-06-18 04:00:30 +0800

    2.1

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let firebase = require("firebase");
 let express = require("express");
 let app = express();
@@ -13,119 +12,109 @@ let port = process.env.PORT || 3000;
 
 
 
-server.listen(port, function() {
-  console.log("Server listening on port %d", port);
-});
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database()
 
+
+
+// create server
+server.listen(port, function() {
+    console.log("Server listening on port %d", port);
+});
+
+
+
+// URL route
 app.use(express.static(__dirname));
 
 app.get('/stop', function (req, res) {
-  process.exit();
+    process.exit();
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/login.html');
+    res.sendFile(__dirname + '/public/login.html');
 });
 
 app.get('/:roomname/:username', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
+
+
+// socket
 io.on("connection", function(socket) {
-  console.log("Connected and ready!");
+    console.log("Connected and ready!");
 
+    // on user join
+    socket.on("join", (data) => {
+        let userName = data.userName;
+        let roomName = data.roomName;
+        console.log(`${userName} joined room: ${roomName}`);
 
-  socket.on("join", (data) => {
-    let userName = data.userName;
-    let roomName = data.roomName;
-    console.log(`${userName} joined room: ${roomName}`);
+        // new message
+        database.ref(`room/${roomName}`).on('child_added', function(childSnapshot, prevChildKey) {
+            if (!childSnapshot) return;
+            let msgData = childSnapshot.val();
 
-    // new message
-    database.ref(`room/${roomName}`).on('child_added', function(childSnapshot, prevChildKey) {
-      if (!childSnapshot) return;
-      let msgData = childSnapshot.val();
+            socket.emit("show message", msgData);
+        });
 
-      socket.emit("show message", msgData);
+        // remove message
+        database.ref(`room/${roomName}`).on('child_removed', function(childSnapshot, prevChildKey) {
+            if (!childSnapshot) return;
+            let msgData = childSnapshot.val();
+
+            socket.emit("hide message", msgData);
+        });
     });
 
-    // remove message
-    database.ref(`room/${roomName}`).on('child_removed', function(childSnapshot, prevChildKey) {
-      if (!childSnapshot) return;
-      let msgData = childSnapshot.val();
 
-      socket.emit("hide message", msgData);
+    // on server recieve message
+    socket.on("send message", (data) => {
+        let time = getTime();
+        let upload = {
+            id: time.full,
+            time: time.short,
+            author: data.author,
+            message: data.message
+        };
+        
+        if (isImageUrl(data.message)) { // message is a image link
+            upload.image = data.message;
+            upload.url = data.message;
+            database.ref(`room/${data.room}/${time.full}`).set(upload);
+     
+        } else if (isUrl(data.message)) { // message is a link
+            upload.url = data.message;
+            database.ref(`room/${data.room}/${time.full}`).set(upload);
+        
+        } else { // only text
+            database.ref(`room/${data.room}/${time.full}`).set(upload);
+        }
+
     });
-  });
-
-
-  // send message
-  socket.on("send message", (data) => {
-    let time = getTime();
-    let upload = {
-      id: time.full,
-      time: time.short,
-      author: data.author,
-      message: data.message
-    };
-    
-    if (isUrl(data.message)) {
-      upload.url = data.message;
-    }
-    if (isImageUrl(data.message)) {
-      upload.image = data.message;
-      upload.url = data.message;
-      database.ref(`room/${data.room}/${time.full}`).set(upload);
-   
-    } else if (isUrl(data.message)) {
-      upload.url = data.message;
-      database.ref(`room/${data.room}/${time.full}`).set(upload);
-    
-    } else {
-      database.ref(`room/${data.room}/${time.full}`).set(upload);
-    }
-
-  });
 });
 
 
 
+// return timestamp in 2 format
 function getTime() {
-  let time = new Date();
-  time.setHours(time.getHours() + 8);
-  let year = time.getFullYear().toString().padStart(4, 0);
-  let month = (time.getMonth()+1).toString().padStart(2, 0);
-  let date = time.getDate().toString().padStart(2, 0);
-  let hour = time.getHours().toString().padStart(2, 0);
-  let min = time.getMinutes().toString().padStart(2, 0);
-  let sec = time.getSeconds().toString().padStart(2, 0);
-  let milSec = time.getMilliseconds().toString().padStart(3, 0);
+    let time = new Date();
+    time.setHours(time.getHours() + 8);
+    let year = time.getFullYear().toString().padStart(4, 0);
+    let month = (time.getMonth()+1).toString().padStart(2, 0);
+    let date = time.getDate().toString().padStart(2, 0);
+    let hour = time.getHours().toString().padStart(2, 0);
+    let min = time.getMinutes().toString().padStart(2, 0);
+    let sec = time.getSeconds().toString().padStart(2, 0);
+    let milSec = time.getMilliseconds().toString().padStart(3, 0);
 
-  return {
-    full: `${year}-${month}-${date}_${hour}:${min}:${sec}_${milSec}`,
-    short: `${hour}:${min}:${sec}`
-  };
+    return {
+        // used for database
+        full: `${year}-${month}-${date}_${hour}:${min}:${sec}_${milSec}`,
+        
+        // used for user
+        short: `${year}-${month}-${date} ${hour}:${min}`
+    };
 }
-
-
-// write
-// database.ref("Test").set("TEST", function(error) {
-//   if (error) {
-//     console.log("Failed with error: " + error)
-//   } else {
-//     console.log("success")
-//   }
-// })
-
-// listener
-// database.ref().on('child_changed', function(childSnapshot, prevChildKey) {
-//   console.log(childSnapshot.val())
-// });
-
-// read once
-// database.ref("Test").once('value', (snapshot) => {
-//   console.log(snapshot.val());
-// });
